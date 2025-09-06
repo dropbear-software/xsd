197521b5b9c4e94eb7be0f52d871181193b6be86
mark <mark@dropbear.dev>
2025-09-06 14:04:14 +0000

    Refactor xsd:short codec implementation

--- lib/src/codecs/short/short_codec.dart
+++ lib/src/codecs/short/short_codec.dart
@@ -8,9 +8,6 @@ const xsdShortCodec = XsdShortCodec();
 class XsdShortCodec extends Codec<int, String> {
   const XsdShortCodec();
 
-  static final BigInt minShortInclusive = BigInt.parse('-32768');
-  static final BigInt maxShortInclusive = BigInt.parse('32767');
-
   @override
   Converter<String, int> get decoder => const XsdShortDecoder();
 
