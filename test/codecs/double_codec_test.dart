06ea9cb7dd5921cb78d0e7bad6a9afca45a09d06
mark <mark@dropbear.dev>
2025-11-27 07:41:27 +0000

    test: Add additional double codec tests

--- test/codecs/double_codec_test.dart
+++ test/codecs/double_codec_test.dart
@@ -11,6 +11,7 @@ void main() {
       expect(codec.encode(double.infinity), 'INF');
       expect(codec.encode(double.negativeInfinity), '-INF');
       expect(codec.encode(double.nan), 'NaN');
+      expect(codec.encode(double.nan), 'NaN');
     });
 
     test('should decode valid double', () {
@@ -21,6 +22,16 @@ void main() {
       expect(codec.decode('NaN').isNaN, isTrue);
       expect(codec.decode('1.2E3'), 1200.0);
       expect(codec.decode('1.2e3'), 1200.0);
+    });
+
+    test('should handle whitespace collapsing', () {
+      expect(codec.decode('  123.45  '), 123.45);
+      expect(codec.decode('\n-123.45\t'), -123.45);
+    });
+
+    test('should throw FormatException for invalid input', () {
+      expect(() => codec.decode('invalid'), throwsFormatException);
+      expect(() => codec.decode('1.2.3'), throwsFormatException);
     });
   });
 }
